18738d10db602eee07cb169dadb23491a67da670
goodgoodman <goodgoodman@goodgoodmanui-MacBookPro-2.local>
2022-09-01 14:59:40 +0700

    zxcvasdf

--- src/routes/temp.js
+++ src/routes/temp.js
@@ -1,7 +1,30 @@
+// import * as puppeteer from 'puppeteer-core'
+import Chromium from "chrome-aws-lambda"
 
 export const get = async () => {
+    const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === 'win32'
+            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+            : process.platform === 'linux'
+            ? '/usr/bin/google-chrome'
+            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      }
+    const browser = await Chromium.puppeteer.launch(options)
+
+    const page = await browser.newPage()
+    await page.setViewport({ width: 2000, height: 1000 });
+    await page.goto("https://sveltekit-temp-test.vercel.app/")
   console.log("temp")
-  const {generatePdf} = await import("html-pdf-node")
+  const pdf =await page.pdf({format: 'a4'})
+  await browser.close()
   /*
   const buffer = await generatePdf(
     { url: "https://sveltekit-temp-test.vercel.app/" },
@@ -15,6 +38,6 @@ export const get = async () => {
       "Content-Disposition": "attachment; filename=temp.pdf",
     },
     //body: buffer,
-    body: "hel",
+    body:  pdf,
   }
 }
