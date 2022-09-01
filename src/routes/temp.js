2cc78f098b17c6538591d549601336b61d95c106
goodgoodman <goodgoodman@goodgoodmanui-MacBookPro-2.local>
2022-09-01 14:43:38 +0700

    asd

--- src/routes/temp.js
+++ src/routes/temp.js
@@ -1,17 +1,20 @@
 
 export const get = async () => {
   console.log("temp")
+  /*
   const {generatePdf} = await import("html-pdf-node")
   const buffer = await generatePdf(
     { url: "https://sveltekit-temp-test.vercel.app/" },
     { format: "A4" }
   )
+  */
   console.log("temp1")
   return {
     headers: {
       "Content-type": "application/pdf",
       "Content-Disposition": "attachment; filename=temp.pdf",
     },
-    body: buffer,
+    //body: buffer,
+    body: "hel",
   }
 }
